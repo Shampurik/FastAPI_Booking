f2da14be0a26c04dc9ae2fd2ca9cec742ba40415
Shampurik <shampuriku@mail.ru>
2023-11-24 15:01:27 +0300

    Add registration prototype

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,7 +1,5 @@
 from app.dao.base import BaseDAO
-from app.database import async_session_maker
 
-from sqlalchemy import select
 from app.bookings.models import Bookings
 
 class BookingDAO(BaseDAO):
--- app/dao/base.py
+++ app/dao/base.py
@@ -1,6 +1,6 @@
 from app.database import async_session_maker
 
-from sqlalchemy import select
+from sqlalchemy import insert, select
 
 
 class BaseDAO:
@@ -26,3 +26,10 @@ class BaseDAO:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().all()
+    
+    @classmethod
+    async def add(cls, **data):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**data)
+            await session.execute(query)
+            await session.commit()
