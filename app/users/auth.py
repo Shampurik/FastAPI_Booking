c30ba7798396753d9d7f0ab87789511595b7658e
Shampurik <shampuriku@mail.ru>
2023-11-26 00:25:17 +0300

    Fix authentication bug

--- app/users/auth.py
+++ app/users/auth.py
@@ -30,6 +30,6 @@ def create_access_token(data: dict) -> str:
 
 async def authenticate_user(email: EmailStr, password: str):
     user = await UsersDAO.find_one_or_none(email=email)
-    if not user and not verify_password(password, user.password):
+    if not user or not verify_password(password, user.hashed_password):
         return None
     return user
