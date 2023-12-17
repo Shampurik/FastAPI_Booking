3c35097a4c79ec02b9c1d4730809195eaa9028f2
Shampurik <shampuriku@mail.ru>
2023-12-17 05:27:35 +0300

    Add /logout, /me endpoints

--- app/users/router.py
+++ app/users/router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 
 from app.users.auth import authenticate_user, create_access_token, get_password_hash
 from app.users.dao import UsersDAO
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 from app.users.schemas import SUserAuth
 
 router = APIRouter(
@@ -28,3 +30,13 @@ async def login_user(response: Response, user_data: SUserAuth):
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return {"access_token": access_token}
+
+
+@router.post("/logout")
+def logout_user(response: Response):
+    response.delete_cookie("booking_access_token")
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
