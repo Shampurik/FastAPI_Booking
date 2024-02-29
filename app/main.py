905693b7553eee4891dc651712b9c9503dc921f2
Shampurik <shampurik04@gmail.com>
2024-02-29 21:45:36 +0300

    Add very simple frontend
    * Created router.py for pages
    * Created simple template to return hotels by location
    * Add very simple GET-endpoint to pages router, to return hotels by location using Jinja2Templates
    * Included router for pages in main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from app.bookings.router import router as router_bookings
 from app.hotels.rooms.router import router as router_rooms
 from app.hotels.router import router as router_hotels
+from app.pages.router import router as router_pages
 from app.users.router import router as router_users
 
 app = FastAPI()
@@ -12,6 +13,7 @@ app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+app.include_router(router_pages)
 
 
 # uvicorn app.main:app --reload
