ac7a8883f2c57a8fa6a67537210b6a0bd579641f
Shampurik <shampurik04@gmail.com>
2024-03-19 03:02:02 +0300

    Add default values to settings

--- app/config.py
+++ app/config.py
@@ -1,15 +1,21 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
+class DbSettings(BaseSettings):
+    DB_HOST: str = Field("localhost", env="DB_HOST")
+    DB_PORT: int = Field(5432, env="DB_PORT")
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
 
+    REDIS_HOST: str = Field("localhost", env="REDIS_HOST")
+    REDIS_PORT: int = Field(6379, env="REDIS_PORT")
+
+
+class Settings(DbSettings):
     SMTP_HOST: str
-    SMTP_PORT: str
+    SMTP_PORT: str = Field(465, env="SMTP_PORT")
     SMTP_USER_EMAIL: str
     SMTP_PASS: str
 
@@ -18,6 +24,7 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+        case_sensitive = False
 
 
 settings = Settings()
